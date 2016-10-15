c5ee678b2198f7466a72fb35673722a67ef2bef1
nelsonabieno <nelsonabieno@gmail.com>
2016-10-14 22:37:36 -0700

    my 6th commit with 8 tests

--- tests.js
+++ tests.js
@@ -16,6 +16,11 @@ describe("Test that sums up all the prime numbers from 1 to n", function() {
   
   });
 
+  it("should output 328 for prime nos between 1 and 50", function() {
+    assert(lib.sumofprimes(50)==328);
+  
+  });
+
   it("should give invalid parameter for a string input", function() {
     assert(lib.sumofprimes("mystring")==false);
   
@@ -31,8 +36,13 @@ describe("Test that sums up all the prime numbers from 1 to n", function() {
   
   });
 
-  it("should output 0 if zero is supplied", function() {
-    assert(lib.sumofprimes(0)==0);
+  it("should give invalid parameter for an object", function() {
+    assert(lib.sumofprimes({1:7})==false);
+  
+  });
+
+  it("should output 16401 if 433 is supplied", function() {
+    assert(lib.sumofprimes(433)==16401);
   
   });
   
